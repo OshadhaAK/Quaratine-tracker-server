fd9b1f43fe57213f57b608d5a13c68eb359522fc
oshadhaAK <oshadhakatulanda.16@cse.mrt.ac.lk>
2020-04-04 16:51:15 -0400

    login response updated

--- routes/users.js
+++ routes/users.js
@@ -98,7 +98,7 @@ router.post('/login', async (req,res) => {
     /* const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
     res.header('auth-token', token).send(token); */
 
-    res.send({ name: user.name});
+    res.send({ name: user.name, message: 'Success'});
 
 });
 
